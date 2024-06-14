281ef22e48b5f09682614a51ebab3e055e950f2f
LoriaAkanksha <akankshaloria21@gmail.com>
2024-06-14 13:29:44 +0530

    training stage is completed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from cnnClassifier.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage2_prepare_base_model import PrepareBaseModelTrainingPipeline
-#from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage3_training import ModelTrainingPipeline
 #from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 from cnnClassifier import logger
 
@@ -16,12 +16,25 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
 STAGE_NAME = "Prepare base model"
 try: 
    logger.info(f"*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
