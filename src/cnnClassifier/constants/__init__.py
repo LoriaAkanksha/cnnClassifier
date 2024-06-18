80063e19beae897cc1fad3ef326ce057b30f5b0d
LoriaAkanksha <akankshaloria21@gmail.com>
2024-06-18 12:56:08 +0530

    data ingestion recreated

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path("/home/akanksha/Desktop/CNN/cnnClassifier/config/config.yaml")
-PARAMS_FILE_PATH = Path("/home/akanksha/Desktop/CNN/cnnClassifier/params.yaml")
+CONFIG_FILE_PATH = Path("/home/akanksha/Desktop/cnnCheck/cnnClassifier/config/config.yaml")
+PARAMS_FILE_PATH = Path("/home/akanksha/Desktop/cnnCheck/cnnClassifier/params.yaml")
